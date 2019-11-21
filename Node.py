3693876f6cbac0400c025b13063dce479fe29e30
AnaJu <56701827+AnaJL@users.noreply.github.com>
2019-11-20 23:39:51 -0300

    Add files via upload

--- Node.py
+++ Node.py
@@ -1,21 +1,5 @@
-from Dados import Dado 
-
 class No:
     def __init__(self, dado):
-        self._dado = dado
-        self._left = None
-        self._right = None
-    def get_dado(self):
-        return self._dado
-    def set_dado(self, novoDado):
-        self._dado = novoDado
-    def get_left(self):
-        return self._left
-    def set_left(self, outro):
-        self._left = outro
-    def get_right(self):
-        return self._right
-    def set_right(self, outro):
-        self._right = outro
-    def __str__(self):
-        return str(self._dado)
+        self.dado = dado
+        self.left = None
+        self.right = None
