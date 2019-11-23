9d13dd9b0dec103646cb5c0ccc931675497ec859
8b1tz <53948477+8b1tz@users.noreply.github.com>
2019-11-22 21:52:04 -0300

    Update tentativamenu.py

--- tentativamenu.py
+++ tentativamenu.py
@@ -1,33 +1,70 @@
-from tkinter import *
-from Dados import *
-from Node import  *
+from tkinter import*
+from Dados import Dado
+from Tree import Avl
+from Node import No
 from random import randint
-def button1_click():
-    filme = ed1.get()
-    ano = ed2.get()
-    chave = randint(1,1000)
-    Dado(filme,ano,chave)
-    lb["text"] = "Inserido com sucesso!"
-def button2_click():
-    print("bt_click")
-janela = Tk() #Cria a janela
-ed1 = Entry(janela)
-ed1.place(x = 100, y =100)
-ed2 = Entry(janela)
-ed2.place(x=100, y= 150)
-ed3 = Entry(janela)
-ed3.place(x=100,y=200)
-janela.title("Árvore AVL")
-bt1 = Button(janela, width = 20, text = "Inserir Filme", command = button1_click)
-bt1.place(x = 200, y = 250)
-lbFilme = Label(janela, text= "Filme: ")
-lbFilme.place(x = 50, y=100)
-lbAno = Label(janela, text= "Ano: ")
-lbAno.place(x=50, y= 150)
-lbChave = Label(janela, text="Chave: ")
-lbChave.place(x=50, y=200)
-lb = Label(janela, text = "A Resposta irá aparecer aqui!")
-lb.place(x=200, y= 50)
-#largura x Altura + Esquerda do vídeo+ Topo do vídeo
-janela.geometry("600x400+400+100")
-janela.mainloop() #Laço de repetição enquanto a janela está aberta
+
+root = Tk()
+root.title("Projeto Árvore AVL")
+arv = Avl(No(Dado()))
+
+def emordem():
+    a = arv.emordem(arv.root)
+    if(a == 'None'):
+        text2.configure(text = a)
+    else:
+        text2.configure(text = a)
+def altura():
+    if(arv.altura(arv.root) == 0):
+        text2.configure(text = '0')
+    else:
+        text2.configure(text = str(arv.altura(arv.root)))
+def inserir():
+    chave = randint(0,10000)
+    text_chave.configure(text= "sua chave é: ")
+    text_chave1.configure(text=chave)
+    if (inp.get() == ""):
+        text2.configure(text = 'você não colocou filme')
+    elif (inp2.get() == ""):
+        text2.configure(text = 'você não colocou ano!')
+    else:
+        a = arv.inserir(No(Dado(inp.get(), inp2.get(),chave)))
+        text2.configure(text = a)
+def alfabeto():
+    text2.configure(text = "a")
+
+text_inp = Label(text = "Filme: ")
+text_inp.grid(column = 1, row = 3)
+text_fila = Label(text = "Ano: ")
+text_fila.grid(column = 1, row = 4)
+text_chave = Label(text = '')
+text_chave.grid(column = 1, row =5 )
+text_chave1 = Label(text = '')
+text_chave1.grid(column = 2, row =5 )
+
+
+inp = Entry()
+inp.grid(column = 2, row = 3)
+inp2 = Entry()
+inp2.grid(column = 2, row = 4)
+
+
+text2 = Label(text = '')
+text2.grid(column = 3, row = 4)
+
+ordem = Button(text = "Em Ordem", command = emordem, width = 15)
+ordem.grid(column = 0, row = 4, padx = 10, pady = 5)
+
+altura = Button(text = "Altura", command = altura, width = 15)
+altura.grid(column = 0, row = 3, padx = 10, pady = 5)
+
+
+inserir = Button(text = "Inserir", command = inserir, width = 15)
+inserir.grid(column = 3, row = 10, padx = 10, pady = 5)
+
+ordenar = Button(text = "Ordem Alfabetica", command = alfabeto, width = 15)
+ordenar.grid(column = 0, row = 5, padx = 10, pady = 5)
+
+
+
+root.mainloop()
