8aae5a2ad5681a457eadcd8cdd402a80fe5489e3
AnaJu <56701827+AnaJL@users.noreply.github.com>
2019-11-23 21:57:05 -0300

    Update Tree.py

--- Tree.py
+++ Tree.py
@@ -1,121 +1,226 @@
-from Node import No
 from Dados import Dado
 
-class avl:
-    def __init__(self, dado = None):
-            self._root = dado
-    def inserir(self, novo):
-        no = No(novo)
-        if self._root == None:
-            self._root = no
-            return 'Foi adicionado com sucesso !'
-        
-        else:
-            atual = self._root
-            while atual != None:
-                ant = atual
-                if novo.get_id() < atual.dado.get_id(): #indo p/ esquerda
-                    atual = atual.left
-                    if atual == None:
-                        ant.left = no
-                elif novo.get_id() > atual.dado.get_id():                                  #indo p/ direita
-                    atual = atual.right
-                    if atual == None:
-                        ant.right = no
-                else:
-                    return f'O ID {novo.get_id()} já existe !'
-            return 'Foi adicionado com sucesso !'
-            
-    def buscaid(self, ide):
-        if self._root == None:
-            print('Arvore Vazia')
-        else:
-            atual = self._root
-            while atual.dado.get_id() != ide:
-                if ide < atual.dado.get_id():
-                    atual = atual.left
-                elif ide > atual.dado.get_id():
-                    atual = atual.right
-                if atual == None:
-                    print('Não encontrada')
-                    break
-            if atual != None:
-                print(atual.dado.get_filmeeano())
-    def buscaano(self, ano):
-        if self._root == None:
-            print('Arvore Vazia')
-        else:
-            atual = self._root
-            while atual.dado.get_ano() != ano:
-                if ano < atual.dado.get_ano():
-                    atual = atual.left
-                elif ano > atual.dado.get_ano():
-                    atual = atual.right
-                if atual == None:
-                    print('Não encontrada')
-                    break
-            if atual != None:
-                print(atual.dado.get_filmeeano())
 
-        
-    def altura(self, atual):  #atual = root, ta na entrada
-        if atual == None:
+class No:
+    def __init__(self, dado=None, ide=None, filho=None, esquerda=None, direita=None):
+        self._dado = dado
+        self._id = ide
+        self._filho = filho
+        self._esquerda = esquerda
+        self._direita = direita
+
+    def get_esquerda(self):
+        return self._esquerda
+
+    def set_esquerda(self, novo):
+        self._esquerda = novo
+
+    def get_direita(self):
+        return self._direita
+
+    def set_direita(self, novo):
+        self._direita = novo
+
+    def profundidade(self):
+        prof_esq = 0
+        prof_dir = 0
+        if self._dado == None:
             return -1
-        elif atual.left == None and atual.right == None:
+        if self._esquerda == None and self._direita == None:
             return 0
         else:
-            if self.altura(atual.left) > self.altura(atual.right):
-                return self.altura(atual.left) + 1
-            else:
-                return self.altura(atual.right) + 1
+            if self.get_esquerda():
+                prof_esq = self.get_esquerda().profundidade()
+            if self.get_direita():
+                prof_dir = self.get_direita().profundidade()
+            return 1 + max(prof_esq, prof_dir)
 
-    def emordem(self, atual): #atual = root, ta na entrada !
-        if atual != None:
-            self.emordem(atual.left)
-            print(f'chave: {atual.dado.get_id()}')
-            self.emordem(atual.right)
-                    
-    def __str__(self):
-        return str(self._dado)
+    def verBalanco(self):
+        profundidade_esquerda = 0
+        profundidade_direita = 0
+        if self.get_esquerda():
+            profundidade_esquerda = self.get_esquerda().profundidade()
+        if self.get_direita():
+            profundidade_direita = self.get_direita().profundidade()
+        return profundidade_esquerda - profundidade_direita
+
+    def Rotacionar_Esquerda(self):
+        aux = self._dado.get_direita()
+        self._dado.set_direita(aux._esquerda())
+        aux.set_esquerda(self._dado)
+        return aux
+
+    def Rotacionar_Direita(self):
+        aux = self._dado.get_esquerda()
+        self._dado.set_esquerda(aux.set_direita())
+        aux.set_direita(self._dado)
+        return aux
+
+    def OrdemAlfa(self, lista):
+        if lista == []:
+            print('Árvore Vazia')
+        else:
+            for i in range(len(lista)):
+                for j in range(len(lista)):
+                    if lista[i] < lista[j]:
+                        lista[i], lista[j] = lista[j], lista[i]
+            print('''ORDEM ALFABÉTICA: ''')
+            for z in range(len(lista)):
+                print(lista[z])
+
+    def inserir(self, elem, pos):
+        if self._dado == None:
+            self._dado = elem
+            return 'Foi adicionado a raiz com sucesso ! ✔'
+        if pos == 'inte':
+            if elem._id < self._id:
+                if not self._esquerda:
+                    self.set_esquerda(elem)
+                else:
+                    self._esquerda.inserir(elem, 'inte')
+                return 'Foi adicionado com sucesso ✔!'
+            else:
+                if not self._direita:
+                    self.set_direita(elem)
+                else:
+                    self._direita.inserir(elem, 'inte')
+                return 'Foi adicionado com sucesso ✔!'
+        if pos == 'ext':
+            if elem._id < self._dado._id:
+                if not self._esquerda:
+                    self.set_esquerda(elem)
+                else:
+                    self._esquerda.inserir(elem, 'inte')
+                return 'Foi adicionado com sucesso ✔!'
+            else:
+                if not self._direita:
+                    self.set_direita(elem)
+                else:
+                    self._direita.inserir(elem, 'inte')
+                return 'Foi adicionado com sucesso ✔ !'
+
+        self.Balanceamento()
+
+    def Rotacionar_Esquerda_Direita(self):
+        self.get_esquerda().Rotacionar_Esquerda()
+        self.Rotacionar_Direita()
+
+    def Rotacionar_Direita_Esquerda(self):
+        self.get_direita().Rotacionar_Direita()
+        self.Rotacionar_Direita()
+
+    def Balanceamento(self):
+        balancear = self.verBalanco()
+        if balancear > 1:
+            if self.get_esquerda().balanco() > 0:
+                self.Rotacionar_Direita()
+            else:
+                self.Rotacionar_Esquerda_Direita()
+        elif balancear < -1:
+            if self.get_direita().balanco < 0:
+                self.Rotacionar_Esquerda()
+            else:
+                self.Rotacionar_Direita_Esquerda()
+
+    def imprimeArvoreAvl(self, pos, espaco=10):
+        if self._dado == None:
+            print('Árvore Vazia !')
+        elif pos == 'ext':
+            print(" " * espaco + str(self._dado._id))
+            if self._esquerda:
+                self._esquerda.imprimeArvoreAvl('inter', espaco - 4)
+            if self._direita:
+                self._direita.imprimeArvoreAvl('inter', espaco + 4)
+        else:
+            print(" " * espaco + str(self._id))
+            if self._esquerda:
+                self._esquerda.imprimeArvoreAvl('inter', espaco - 4)
+            if self._direita:
+                self._direita.imprimeArvoreAvl('inter', espaco + 4)
+
+    def buscaid(self, chave, chaves, lista):
+        Inf = 0
+        Sup = len(chaves) - 1
+        met = 0
+        while (Inf <= Sup):
+            met = (Inf + Sup) // 2
+            if (chaves[met] == chave):
+                return lista[met].get_filmeeano()
+            if (chave < chaves[met]):
+                Sup = met - 1
+            else:
+                Inf = met + 1
+
+        return 'Chave não encontrada'
+    def buscaano(self, ano, anos, lista):
+        Inf = 0
+        Sup = len(anos) - 1
+        met = 0
+        while (Inf <= Sup):
+            met = (Inf + Sup) // 2
+            if (anos[met] == ano):
+                return lista[met].get_filmeeano()
+            if (ano < anos[met]):
+                Sup = met - 1
+            else:
+                Inf = met + 1
+
+        return 'Chave não encontrada'
+
     def menu(self):
         return input(""" 
-0) sair do Menu
-1) Inserir filme
-2) Buscar filme pelo id    
-3) Buscar filmes pelo ano  
-4) Listar filmes em ordem alfabética 
-5) Altura da árvore 
-6) Exibir a árvore
+ _____________________________________________
+|         ▒█▀▄▀█ ▒█▀▀▀ ▒█▄░▒█ ▒█░▒█           |
+|         ▒█▒█▒█ ▒█▀▀▀ ▒█▒█▒█ ▒█░▒█           |
+|         ▒█░░▒█ ▒█▄▄▄ ▒█░░▀█ ░▀▄▄▀           |                                
+| ____________________________________________|                                            
+| 0) sair do Menu                             |
+| 1) Inserir filme                            |
+| 2) Buscar filme pelo id                     |
+| 3) Buscar filmes pelo ano                   |
+| 4) Listar filmes em ordem alfabética        |
+| 5) Altura da árvore                         |
+| 6) Exibir a árvore                          |
+|_____________________________________________|
 
-Digite sua opção: 
+ ▸ Digite sua opção: 
 """)
-        
-          
-arv = avl()
+
+
+arv = No()
 resp = arv.menu()
+listachaves = []
+listaanos = []
+listafilmes = []
+listaD = []
 while resp != '0':
-    if resp == '1' :
+    if resp == '1':
         filme = input('Filme: ')
         ano = int(input('Ano: '))
         chave = int(input('Chave: '))
-        novo = Dado(filme, ano, chave)
-        print(arv.inserir(novo))
-    if resp == '2' :
-        chave = int(input('Chave Buscada: '))
-        arv.buscaid(chave)
-    if resp == '3' :
-        ano = int(input('Ano Buscado: '))
-        arv.buscaano(ano)
-    if resp == '4' :
-        a = None
-        print(arv.OrdemAlfa(a))
-    if resp == '5' :
-        print(f'Altura: {arv.altura(arv._root)}')
-    if resp == '6' :
-        arv.emordem(arv._root)
-    if resp not in ['0','1','2','3','4','5','6']:
-        print('Comando invalido')
+        novo = No(Dado(filme, ano), chave)
+        if chave in listachaves:
+            print(f'o ID {chave} já existe !')
+        else:
+            listaD.append(Dado(filme, ano))
+            listafilmes.append(filme)
+            listachaves.append(chave)
+            listaanos.append(ano)
+            print(arv.inserir(novo, 'ext'))
+    if resp == '2':
+        ide = int(input('ID procurado : '))
+        print(arv.buscaid(ide, listachaves, listaD))
+    if resp == '3':
+        ano = int(input('ANO procurado : '))
+        print(arv.buscaano(ano, listaanos, listaD))
+    if resp == '4':
+        arv.OrdemAlfa(listafilmes)
+    if resp == '5':
+        print(arv.profundidade())
+    if resp == '6':
+        arv.imprimeArvoreAvl('ext')
+    if resp not in ['0', '1', '2', '3', '4', '5', '6']:
+        print('✘ Comando invalido ✘')
     resp = arv.menu()
 print('Fim')
-
 
